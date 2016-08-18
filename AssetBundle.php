0413301d04d63f9ea8889b07dcfc0f643452132d
AIZAWA Hina <hina@bouhime.com>
2016-08-18 22:28:35 +0900

    use official distribution
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- AssetBundle.php
+++ AssetBundle.php
@@ -13,11 +13,10 @@ namespace rmrevin\yii\fontawesome;
  */
 class AssetBundle extends \yii\web\AssetBundle
 {
-
     /**
      * @inherit
      */
-    public $sourcePath = '@vendor/rmrevin/yii2-fontawesome/assets';
+    public $sourcePath = '@vendor/fortawesome/font-awesome';
 
     /**
      * @inherit
